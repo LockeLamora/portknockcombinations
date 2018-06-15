c1604f6e3e45676361a568b15fe24678930b9893
Alick Gardiner <alick.gardiner@performancehorizon.com>
2018-06-15 10:37:38 +0100

    make it faster by removing nmap from the knocking:

--- portknockcombinations.py
+++ portknockcombinations.py
@@ -1,6 +1,6 @@
 import sys
 from itertools import permutations
-from pprint import pprint as pp
+import socket
 import os
 
 def main(args):
@@ -11,32 +11,34 @@ def main(args):
     paramcheck(args)
 
     knocklist =  list(permutations(args))
-    command = generate_commands(ip_address, knocklist)
-    command = execute_final_scan(command, ip_address)
-    execute_command(command)
+    doscan(ip_address, knocklist)
+    execute_final_scan(ip_address)
+
 
 def paramcheck(params):
     if len(params) < 2:
         print('Not enough ports passed')
         exit()
 
-def generate_commands(ip_address, knocklist):
-    command = ''
+def doscan(ip_address, knocklist):
+    tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     for knock in knocklist:
         for port in knock:
             if 'u' in port:
                 port = port.replace('u','')
-                command += 'nmap --max-retries 0 -sU -Pn -p '+str(port)+' '+ip_address+' && '+' sleep 1 && '
+                try:
+                    udp.connect((ip_address, int(port)))
+                except:
+                    pass
             else:
-                command += 'nmap --max-retries 0 -Pn -p '+str(port)+' '+ip_address+' && '+' sleep 1 && '
-    return command
+                try:
+                    tcp.connect((ip_address, int(port)))
+                except:
+                    pass
 
-def execute_final_scan(command, ip_address):
-    return command+'nmap '+ip_address
-
-def execute_command(command):
-    os.system(command)
-
+def execute_final_scan(ip_address):
+    os.system('nmap '+ip_address)
 
 if __name__ == "__main__":
    main(sys.argv[1:])
